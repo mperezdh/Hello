986153b2e8c0d688bfffbea50f7209c38d16c209
mperezdh <mperez@digitalhouse.com>
2021-05-20 10:57:31 -0300

    new mens

--- src/com/meli/Main.java
+++ src/com/meli/Main.java
@@ -3,6 +3,10 @@ package com.meli;
 public class Main {
 
     public static void main(String[] args) {
+        hola();
+    }
+
+    private static void hola() {
         System.out.println("Hola mundo");
     }
 }
